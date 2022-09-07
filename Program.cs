5e9d9e9953c051140afb660d23798b532b9dc0ff
Cüneyt Şener <cuneytsener53@gmail.com>
2022-09-07 16:05:58 +0300

    Klavyeden girilen n kadar sayının ortalamsını alan bir örnek

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // DİZİ TANIMLAMA
+
+            string [] renkler = new string[5];
+
+            string[] hayvanlar = {"Kedi","Köpek","Kuş","Maymun"};
+
+            int [] dizi;
+            dizi = new int[5];
+
+            //Dizilere değer atama ve erişim 
+            renkler[0] = "Mavi";
+            dizi[3] = 10;
+
+
+            Console.WriteLine(hayvanlar[1]);            
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+
+
+            // DÖngüler ve dizi kullanımı
+            //Klavyeden girilen n tane sayının ortalamasını alan bir örnek
+
+            Console.Write("Lütfen eleman sayisini giriniz :");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int [] sayıdizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}. sayıyı giriniz", i+1);
+                sayıdizisi[i] = int.Parse(Console.ReadLine());
+            }
+            int toplam = 0;
+            foreach (var sayi in sayıdizisi)            
+                toplam += sayi;
+
+                Console.WriteLine("Ortalama :" + toplam/diziUzunlugu);
+            
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,4 @@
+Bu projede dizilerin kullanımı hakkında örnekler yaptım.
+
+
+Ve projede klavyeden girilen n kadar sayının ortalamasını alıp ekrana yazdıran bir program yaptım.
